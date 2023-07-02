78119b60cc5da36b28e9c421e0b71be5b8af0c55
Berat Paban <beratpaban@gmail.com>
2023-07-02 19:04:46 +0300

    LocalStorage dersleri tamamlandi

---
+++ 05-local-storage.js
@@ -0,0 +1,22 @@
+let user={userName:"Berat Paban",isActive:true}
+localStorage.setItem('userInfo',user)
+let userInfo=localStorage.getItem('userInfo')
+console.log(userInfo)
+
+//localStorage Bölüm Sonu
+
+let counter=localStorage.getItem('counter') ? Number(localStorage.getItem('counter')): 0
+let counterDOM=document.querySelector('#counter')
+let increaseDOM=document.querySelector('#increase')
+let decreaseDOM=document.querySelector('#decrease')
+
+counterDOM.innerHTML=counter
+
+increaseDOM.addEventListener("click",clickEvent)
+decreaseDOM.addEventListener("click",clickEvent)
+function clickEvent(){
+    
+    this.id=="increase" ? counter +=1: counter -=1
+    localStorage.setItem('counter',counter)
+    counterDOM.innerHTML=counter
+}
