607bb989b26a9c341e6f672fe2ea32b11613504d
Berat Paban <beratpaban@gmail.com>
2023-07-07 01:39:42 +0300

    object lessons finished

---
+++ 09-object.js
@@ -0,0 +1,17 @@
+//object 
+
+let arrObj=[1,2,3]
+let object1={obj:1}
+
+console.log("arrObj",typeof(arrObj))
+console.log("object1",typeof(object1))
+console.log("object1",object1)
+
+let item1=new Object()
+let item2={}
+
+console.log("item1",typeof(item1))
+console.log("item2",typeof(item2))
+
+let item3={}
+let item4=new Object()
---
+++ 10-try-catch.js
@@ -0,0 +1,13 @@
+let products;
+
+try{
+    products.foreach(item=>console.log(item));
+}catch(error){
+    console.log("error:",error)
+    //products=[1,2,3]
+    //products.foreach(item=>console.log(item));
+}
+
+console.log("hata yönetimi düzgün çalışıyor")
+let info="kodluyoruz"
+console.log(info)
